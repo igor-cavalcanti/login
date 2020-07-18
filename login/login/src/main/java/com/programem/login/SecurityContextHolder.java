b410f37607f97426b782471ab775c90b04f409ba
Igor Cavalcanti <igorcavlcanti@gmail.com>
2020-07-18 15:02:17 -0300

    login + pagepessoal

---
+++ login/login/src/main/java/com/programem/login/SecurityContextHolder.java
@@ -0,0 +1,9 @@
+package com.programem.login;
+
+public class SecurityContextHolder {
+
+	public static Object getContext() {
+		return null;
+	}
+
+}
---
+++ login/login/src/main/java/com/programem/login/SecurityWebConfig.java
@@ -0,0 +1,24 @@
+package com.programem.login;
+
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+@EnableWebSecurity
+public class SecurityWebConfig extends WebSecurityConfigurerAdapter {
+   
+  @Override
+  protected void configure(AuthenticationManagerBuilder builder) throws Exception {
+   builder
+      .inMemoryAuthentication()
+      .withUser("marcos").password("{noop}marcos")
+        .roles("USER");
+        
+          
+  
+  }   
+ 
+}
+
